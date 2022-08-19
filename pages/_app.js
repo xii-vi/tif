aaf08dddba1baf87481bbb971e51940648089d59
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-19 13:45:25 +0530

    setup: app setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider,CSSReset } from '@chakra-ui/react'
+import {theme} from "../theme"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   
