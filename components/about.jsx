1d065c2d4441dc2308816aa66911539511b4f912
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-08-19 14:02:41 +0530

    fix: vercel deployment error

--- components/about.jsx
+++ components/about.jsx
@@ -29,7 +29,7 @@ export const About = () => {
             </Heading>
             <Text >
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              industry. Lorem Ipsum has been the industrys standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. t has survived
               not only five centuries.
